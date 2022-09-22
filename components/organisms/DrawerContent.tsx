3727825ea922211b5a2849a914e9297fe64ba672
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2022-09-22 12:51:11 +0900

    アイコン追加

--- components/organisms/DrawerContent.tsx
+++ components/organisms/DrawerContent.tsx
@@ -1,4 +1,4 @@
-import { Appbar, Drawer, Menu } from "react-native-paper";
+import { Appbar, Divider, Drawer, Menu } from "react-native-paper";
 import { DrawerContentScrollView } from "@react-navigation/drawer";
 import { useNavigation, useNavigationState } from "@react-navigation/native";
 import { RootStackParamList } from "../../type/routing.type";
@@ -16,11 +16,14 @@ export const DrawerContent = (props: any) => {
       <Drawer.Section title="">
         <Drawer.Item
           label="検索"
+          icon="search-web"
           // active={nowLocation === "Search"}
           onPress={() => navigation.navigate("Search")}
         />
+        <Divider />
         <Drawer.Item
           label="保存したページ"
+          icon="book-open-page-variant"
           // active={nowLocation === "PageList"}
           onPress={() => navigation.navigate("PageList")}
         />
