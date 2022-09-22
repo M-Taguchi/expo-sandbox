c42a42999b11ee367bab280ad732ab49e8f81da5
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2022-09-22 13:40:44 +0900

    webviewでバックボタンを使用できるように修正

--- components/screen/SearchScreen.tsx
+++ components/screen/SearchScreen.tsx
@@ -1,9 +1,29 @@
-import { Platform } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import { useEffect, useRef, useState } from "react";
+import { BackHandler, Platform } from "react-native";
 import WebView from "react-native-webview";
 import { useWebView } from "../../hooks/useWebView";
 
 export const SearchScreen = () => {
   const { setPageInfo } = useWebView();
+  const [canGoBack, setCanGoBack] = useState(false);
+  const ref = useRef<WebView>(null);
+
+  const onBack = () => {
+    if (canGoBack) {
+      ref.current?.goBack();
+      return true;
+    }
+
+    return false;
+  };
+
+  useFocusEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", onBack);
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", onBack);
+    };
+  });
 
   return Platform.OS === "web" ? (
     <>
@@ -12,8 +32,10 @@ export const SearchScreen = () => {
   ) : (
     <>
       <WebView
+        ref={ref}
         source={{ uri: "https://www.google.com/" }}
         onLoadProgress={(e) => {
+          setCanGoBack(e.nativeEvent.canGoBack);
           setPageInfo({
             url: e.nativeEvent.url,
             title: e.nativeEvent.title,
